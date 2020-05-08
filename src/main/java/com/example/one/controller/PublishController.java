54d0ac35703d99ac75a2fa624b031c84890ab235
YVictor13 <2357772033@qq.com>
2020-05-08 19:25:45 +0800

    Update QuestionMapper

--- src/main/java/com/example/one/controller/PublishController.java
+++ src/main/java/com/example/one/controller/PublishController.java
@@ -1,13 +1,64 @@
 package com.example.one.controller;
 
+import com.example.one.Model.Question;
+import com.example.one.Model.User;
+import com.example.one.mapper.QuestionMapper;
+import com.example.one.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PublishController {
+    @Autowired(required = false)
+    private QuestionMapper questionMapper;
+    @Autowired(required = false)
+    private UserMapper userMapper;
+
     @GetMapping("/publish")
     public String publish(){
         return "publish";
     }
 
+    @PostMapping("/publish")
+    public String doPublish(
+            @RequestParam("title")String title,
+            @RequestParam("description")String description,
+            @RequestParam("tag")String tag,
+            HttpServletRequest request,
+            Model model){
+        User user = null;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                user = userMapper.findByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
+        if(user==null){
+            model.addAttribute("error","*用户未登陆");
+            return "publish";
+        }
+
+        Question question = new Question();
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+        question.setCreator(user.getId());
+        question.setGmtCreate(System.currentTimeMillis());
+        question.setGmtModified(question.getGmtCreate());
+        questionMapper.create(question);
+//        返回首页
+            return "redirect:/";
+    }
 }
---
+++ src/main/java/com/example/one/mapper/QuestionMapper.java
@@ -0,0 +1,11 @@
+package com.example.one.mapper;
+
+import com.example.one.Model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface QuestionMapper {
+    @Insert("INSERT INTO user.question (title,description,gmt_create,gmt_modified,creator,tag) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag}")
+     void create(Question question);
+}
