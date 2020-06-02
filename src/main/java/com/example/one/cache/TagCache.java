936c74c8d966feb0555eae7595a06de81ffdb6da
YVictor13 <2357772033@qq.com>
2020-06-02 11:53:56 +0800

    add TagCache

--- src/main/java/com/example/one/cache/TagCache.java
+++ src/main/java/com/example/one/cache/TagCache.java
@@ -1,4 +1,17 @@
 package com.example.one.cache;
 
+import com.example.one.Dto.TagDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TagCache {
+    public List<TagDTO>  get(){
+        List<TagDTO> tagDTOs = new ArrayList<>();
+        TagDTO tagDTO = new TagDTO();
+        tagDTO.setCategoryName("开发语言");
+        tagDTO.setTags();
+        tagDTOs.add(tagDTO );
+        return tagDTOs;
+    }
 }
